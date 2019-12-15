16a437043f16506f7a352a62901673880630b577
nataliarosa88 <nataliag1988@gmail.com>
2019-12-14 21:38:52 -0300

    feat: [Task 001.001.001] Implementar método salvar Usuário

--- src/main/java/br/com/todospodemprogramar/app/controller/RestController.java
+++ src/main/java/br/com/todospodemprogramar/app/controller/RestController.java
@@ -1,10 +1,27 @@
 package br.com.todospodemprogramar.app.controller;
 
+import br.com.todospodemprogramar.app.model.User;
+import br.com.todospodemprogramar.app.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Natalia Rosa on 14/12/19.
  */
-
+@Controller
+@RequestMapping("/v2/users")
 public class RestController {
 
+    @Autowired
+    private UserService userService;
 
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.saveMyUser(user));
+    }
 }
